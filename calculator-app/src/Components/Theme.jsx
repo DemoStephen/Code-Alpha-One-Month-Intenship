850c7fde0bd684dae921fff0a311db2e9a0768e5
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-06 20:24:48 +0100

    updated theme section

--- calculator-app/src/Components/Theme.jsx
+++ calculator-app/src/Components/Theme.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/prop-types */
 export default function Theme({ label }) {
   return (
-    <label htmlFor={`theme-${label}`}>
-      <input type="radio" name="theme" id={`theme-${label}`} />
-      <span>{label}</span>
+    <label htmlFor={`theme-${label}`} className="cursor-pointer">
+      <input
+        type="radio"
+        name="theme"
+        id={`theme-${label}`}
+        className="hidden peer"
+        defaultChecked={label === 1}
+      />
+      <span className="w-3 h-3 peer-checked:bg-[#d03f2f] rounded-full flex text-[0.55rem] items-center content-center justify-center group">
+        <span className="hidden group-hover:block">{label}</span>
+      </span>
     </label>
   );
 }
--- calculator-app/src/Components/ThemeChanger.jsx
+++ calculator-app/src/Components/ThemeChanger.jsx
@@ -5,12 +5,18 @@ export default function ThemeChanger() {
     <form className="flex items-end gap-4">
       <p className="text-sm">THEME</p>
       <fieldset>
-        <div className="flex text-xs">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
+        <div className="flex justify-between text-[0.65rem] px-2 py-0.5 rounded-xl">
+          <label htmlFor="theme-1" className="cursor-pointer">
+            1
+          </label>
+          <label htmlFor="theme-2" className="cursor-pointer">
+            2
+          </label>
+          <label htmlFor="theme-3" className="cursor-pointer">
+            3
+          </label>
         </div>
-        <div>
+        <div className="flex gap-1 py-0.5 px-1 bg-[#637097] rounded-xl">
           <Theme label={1} />
           <Theme label={2} />
           <Theme label={3} />
