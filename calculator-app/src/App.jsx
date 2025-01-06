3c12e42a70108610d9b927e6ed0e209dffd62ec8
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-06 20:26:37 +0100

    Working on updating the output screen

--- calculator-app/src/App.jsx
+++ calculator-app/src/App.jsx
@@ -5,15 +5,30 @@ import ResultScreen from "./Components/ResultScreen";
 
 export default function App() {
   const [values, setValues] = useState({
-    currentValues: "0",
-    previousValues: "0",
+    current: "",
+    previous: "",
+    operator: undefined,
   });
-
+  function updateScreen({ current, previous, operator }) {
+    if (operator)
+      setValues((prev) => ({
+        ...prev,
+        current: current,
+        previous: previous,
+      }));
+    else {
+      setValues((prev) => ({
+        ...prev,
+        current: current,
+        previous: "",
+      }));
+    }
+  }
   return (
-    <main className="rounded-lg px-5 py-8 bg-[#3a4764] flex flex-col gap-4 w-11/12 md:w-[25rem]">
+    <main className="rounded-lg px-5 py-8 bg-[#3a4764] flex flex-col gap-4 w-11/12 sm:w-[25rem]">
       <Navigation />
       <ResultScreen output={values} />
-      <InputButtons />
+      <InputButtons handleUpdate={updateScreen} />
     </main>
   );
 }
