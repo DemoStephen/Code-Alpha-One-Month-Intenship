7cbda979a969851514cd3af1b7173453127fe807
Adewale Stephen <adewalestephennifemi@gmail.com>
2025-01-06 20:26:05 +0100

    Result screen updated

--- calculator-app/src/Components/ResultScreen.jsx
+++ calculator-app/src/Components/ResultScreen.jsx
@@ -2,8 +2,11 @@
 export default function ResultScreen({ output }) {
   return (
     <section className="text-end bg-[#182034] text-white p-4 rounded-md">
-      <p className="text-xl">{output.currentValues}</p>
-      <h3 className="text-5xl">{output.previousValues}</h3>
+      <p className="text-xl">{output.current}</p>
+      <h3 className="text-5xl flex justify-between">
+        <sup className="text-xs">{output.previous && "Ans"}</sup>
+        <span>{output.previous}</span>
+      </h3>
     </section>
   );
 }
